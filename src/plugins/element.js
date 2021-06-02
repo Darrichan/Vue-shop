5de4b788a2b4afa6cb0795cda9692d24457a81fa
darrichan <770543388@qq.com>
2021-06-02 11:08:18 +0800

    '完成了login功能'

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
+import { Form, formItem, input, Message } from 'element-ui'
 
+Vue.use(formItem)
+Vue.use(Form)
+Vue.use(input)
 Vue.use(Button)
+Vue.prototype.$message = Message
